618783115b6d00bc17a5c5a08ac0c212067cc885
psodiam <sodiam.app@gmail.com>
2022-07-05 20:34:32 +0700

    fixbug tag on dashboard file

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div>ยังไม่พร้อมใช้งาน</div>
   <div v-if="false">
     <WidgetsStatsA />
-    <CRow></CRow>
+    <CRow>
       <CCol :md="12">
         <CCard class="mb-4">
           <CCardBody>
